0b3f723791b8684ed8a23c6841e80c6026a2d16f
wahyuutami <wahyu_utami_26rpl@student.smktelkom-mlg.sch.id>
2019-04-01 19:47:43 +0700

    selesai

---
+++ app/src/main/java/intent/edu/dycode/com/intent_wahyu/TargetActivity.java
@@ -0,0 +1,19 @@
+package intent.edu.dycode.com.intent_wahyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TargetActivity extends AppCompatActivity {
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout. activity_target );
+        txtResult = (TextView)findViewById(R.id.textView);
+        //get data
+        if(getIntent().getExtras() != null ){
+            String data = getIntent().getExtras().getString(MainActivity. EXTRA_DATA );
+            txtResult.setText(data);
+    }
+} }
